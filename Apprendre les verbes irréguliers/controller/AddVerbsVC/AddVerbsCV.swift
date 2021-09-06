36698e4b0489073e75f024ffbd07ddf4104dcb4c
Lauriane Mollier <mollierlauriane@gmail.com>
2021-09-06 17:17:16 +0200

    WIP

--- Apprendre les verbes irréguliers/controller/AddVerbsVC/AddVerbsCV.swift
+++ Apprendre les verbes irréguliers/controller/AddVerbsVC/AddVerbsCV.swift
@@ -20,10 +20,6 @@ class AddVerbsCV: UIViewController {
     
     let step: Int = 1
     
-    var nbrRandomVerb: Int!
-    var nbrNotSeenVerb: Int!
-    
-    
     // ------------------
     // MARK: - Outlets
     // ------------------
@@ -32,79 +28,17 @@ class AddVerbsCV: UIViewController {
 
     @IBOutlet weak var leftPopUpConstraint: NSLayoutConstraint!
     
-    @IBOutlet weak var addManuallyButton: UIButton!
-    
     @IBOutlet weak var A2Button: LevelCheckBox!
     @IBOutlet weak var B1Button: LevelCheckBox!
     @IBOutlet weak var B2Button: LevelCheckBox!
     @IBOutlet weak var C1Button: LevelCheckBox!
-    
-    @IBOutlet weak var addRandomlyButton: BasicButton!
-    
-
-    @IBOutlet weak var lessRandomVerbButton: UIButton!
-    @IBOutlet weak var moreRandomVerbButton: UIButton!
-    @IBOutlet weak var nbrRandomVerbLabel: UILabel!
     
     
     // ------------------
     // MARK: - Actions
     // ------------------
     
-    @IBAction func back(_ sender: UIButton) {
-        back()
-    }
-    
-    @IBAction func lessRandomVerb(_ sender: UIButton) {
-        self.nbrRandomVerb = [self.nbrRandomVerb - self.step, 0].max()
-        self.nbrRandomVerbLabel.text = String(self.nbrRandomVerb)
-    }
-    
-    @IBAction func moreRandomVerb(_ sender: UIButton) {
-        self.nbrRandomVerb = [self.nbrRandomVerb + self.step, self.nbrNotSeenVerb].min()!
-        self.nbrRandomVerbLabel.text = String(self.nbrRandomVerb)
-    }
-    
-    @IBAction func addRamdomly(_ sender: BasicButton) {
-        let selectedLevels = self.selectedLevels()
-        if selectedLevels.isEmpty{
-            showPopUp()
-        } else {
-            addVerb()
-        }
-        
-//        let selectedLevels = self.selectedLevels()
-//        if selectedLevels.isEmpty{
-//            showPopUp()
-//        } else if self.wholeAppProduct == nil && BoughtIAPProducts.shared.isBought(productIdentifier: IAProducts.wholeApp){
-//            addVerb()
-//        } else if self.wholeAppProduct == nil && !NetworkActivityIndicatorManagment.isInternetAvailable() {
-//            // Internet is not available
-//            // TODO
-//            let alert = UIAlertController(title: "Pas de connection internet",
-//                                          message: "Cette partie de l'application requiert une connection à l'AppStore", preferredStyle: .alert)
-//
-//            let clearAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
-//            alert.addAction(clearAction)
-//            present(alert, animated: true, completion: nil)
-//
-//        } else if self.wholeAppProduct == nil{
-//            // It has to wait for the product
-//        } else if self.wholeAppProduct != nil && IAProducts.store.isProductPurchased(self.wholeAppProduct!.productIdentifier) {
-//            addVerb()
-//        } else if IAPHelper.canMakePayments() {
-//            performSegue(withIdentifier: "IAPurchasesSegue", sender: nil)
-//        } else {
-//            // can NOT make payement
-//            // TODO
-//            let alert = UIAlertController(title: "Permission refusée", message: "Si vous voyez cette page, s'il vous plaît contactez nous. L'AppStore devrait authorisé toute personne de 4 ans et plus", preferredStyle: .alert)
-//
-//            let clearAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
-//            alert.addAction(clearAction)
-//            present(alert, animated: true, completion:nil)
-//        }
-    }
-    
+
     @IBAction func closePopUp(_ sender: UIButton) {
         closePopUp()
     }
@@ -124,7 +58,6 @@ class AddVerbsCV: UIViewController {
         self.B1Button.setup()
         self.B2Button.setup()
         self.C1Button.setup()
-        self.addRandomlyButton.layout()
     }
     
     override func systemLayoutFittingSizeDidChange(forChildContentContainer container: UIContentContainer) {
@@ -133,18 +66,6 @@ class AddVerbsCV: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadProduct() // purchases
-        
-        // TODO: When there is no verb to add
-        do {
-            self.nbrNotSeenVerb = try DAO.shared.verNotInReviewListCount()
-            self.nbrRandomVerb = [10, nbrNotSeenVerb].min()
-            self.nbrRandomVerbLabel.text = String(self.nbrRandomVerb)
-        }
-        catch{
-            // TODO
-            SpeedLog.print(error)
-        }
     }
     
 
@@ -184,22 +105,22 @@ class AddVerbsCV: UIViewController {
     }
     
     private func addVerb(){
-        let selectedLevels = self.selectedLevels()
-        if selectedLevels.isEmpty{
-            performSegue(withIdentifier: "SelectLevelSeque", sender: nil)
-        }else{
-            do{
-                try DAO.shared.addRandomVerbToReviewList(ofLevel: selectedLevels, count: self.nbrRandomVerb)
-                _ = self.navigationController?.navigationController?.popViewController(animated: true)
-                
-                navigationController?.popViewController(animated: true)
-                dismiss(animated: true, completion: nil)
-            }
-            catch{
-                // TODO
-                SpeedLog.print(error)
-            }
-        }
+//        let selectedLevels = self.selectedLevels()
+//        if selectedLevels.isEmpty{
+//            performSegue(withIdentifier: "SelectLevelSeque", sender: nil)
+//        }else{
+//            do{
+//                try DAO.shared.addRandomVerbToReviewList(ofLevel: selectedLevels, count: self.nbrRandomVerb)
+//                _ = self.navigationController?.navigationController?.popViewController(animated: true)
+//                
+//                navigationController?.popViewController(animated: true)
+//                dismiss(animated: true, completion: nil)
+//            }
+//            catch{
+//                // TODO
+//                SpeedLog.print(error)
+//            }
+//        }
     }
     
     // https://medium.com/@mohammadhemani/set-the-next-view-controller-to-show-as-full-screen-in-ios-13-e7e42e20587a
